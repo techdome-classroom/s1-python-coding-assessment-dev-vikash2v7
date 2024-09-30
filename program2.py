4382df968bbc5ef2f33d9e85688e0e4863721fd1
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 12:35:02 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -11,7 +11,7 @@ def decode_message( s: str, p: str) -> bool:
         # Handle patterns starting with '*' that can match an empty message
         for i in range(1, n + 1):
                 if p[i - 1] == '*':
-                dp[i][0] = dp[i - 1][0]
+                 dp[i][0] = dp[i - 1][0]
         
         # Fill the dp table
         for i in range(1, n + 1):
