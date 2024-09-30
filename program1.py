f1bc96ca38a96f376750f163d5582038c1daff16
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 12:22:03 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -6,13 +6,15 @@ class Solution:
             return 0
 
         def dfs(grid, i, j):
+            
             if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
                 return
+
             grid[i][j] = 'W'
-            dfs(grid, i + 1, j)  # down
-            dfs(grid, i - 1, j)  # up
-            dfs(grid, i, j + 1)  # right
-            dfs(grid, i, j - 1)  # left
+            dfs(grid, i + 1, j)  
+            dfs(grid, i - 1, j)  
+            dfs(grid, i, j + 1)  
+            dfs(grid, i, j - 1)  
 
         island_count = 0
         for i in range(len(grid)):
