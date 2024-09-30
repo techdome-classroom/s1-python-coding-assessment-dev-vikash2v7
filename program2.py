09f85b55f6be0f125864a96d98366c799649039f
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 12:38:30 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -18,9 +18,7 @@ def decode_message(message: str, pattern: str) -> bool:
     for i in range(1, pattern_len + 1):
         for j in range(1, message_len + 1):
             # If the current pattern character is '*', it can either:
-            # - Ignore the current message character and move on (dp[i-1][j])
-            # - Match the current message character and continue (dp[i][j-1])
-
+         
             if pattern[i - 1] == '*':
                 dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
 
