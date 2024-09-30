697ccb76408c1d9f0a3169c27ee05f7b8449a66d
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 12:31:32 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -16,12 +16,13 @@ def decode_message( s: str, p: str) -> bool:
         
         # Fill the dp table
         for i in range(1, p + 1):
-                
+
                 for j in range(1, m + 1):
 
                 if pattern[i - 1] == '*':
                         # '*' can match zero or more characters
                         dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+                        
                 elif pattern[i - 1] == '?' or pattern[i - 1] == message[j - 1]:
                         # '?' matches any single character or exact match of current character
                         dp[i][j] = dp[i - 1][j - 1]
