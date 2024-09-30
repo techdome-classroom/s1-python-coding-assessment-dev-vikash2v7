71e7635f99a2a10df261ed401108dfed701be0e2
dev-vikash2v7 <vikashvermacom92@gmail.com>
2024-09-30 12:22:25 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,16 +1,16 @@
 class Solution:
    
    
-        def dfs(grid, i, j):
-            
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
-                return
+    def dfs(grid, i, j):
+        
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 'W':
+            return
 
-            grid[i][j] = 'W'
-            dfs(grid, i + 1, j)  
-            dfs(grid, i - 1, j)  
-            dfs(grid, i, j + 1)  
-            dfs(grid, i, j - 1)  
+        grid[i][j] = 'W'
+        dfs(grid, i + 1, j)  
+        dfs(grid, i - 1, j)  
+        dfs(grid, i, j + 1)  
+        dfs(grid, i, j - 1)  
 
     def getTotalIsles(self, grid: list[list[str]]) -> int:
 
